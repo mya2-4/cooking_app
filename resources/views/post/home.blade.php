66c70a26b2273f371bd8f038d9b4442e73842eaf
mya2-4 <k.mameharu@gmail.com>
2026-07-03 19:19:30 +0900

    update something

--- resources/views/post/home.blade.php
+++ resources/views/post/home.blade.php
@@ -11,5 +11,29 @@
     
 </html>
 <body>
-    <div class="logo">logo</div>
+    <div class="logo font-bold pt-30 text-4xl flex justify-center">logo</div>
+    
+    <div class="mt-20 flex flex-col items-center gap-10">
+
+        <button class="flex justify-center rounded-[20px] w-[330px] h-[100px] bg-[#FA897B] text-white">
+            <div class="w-1/2 flex justify-center items-center">
+                <span class="font-bold text-2xl">マイレシピ</span>
+            </div>
+            <div class="w-1/2 flex justify-center items-center">
+                <span class="countrecipies font-bold text-base">fff</span>
+                <span class="font-bold text-base">recipies</span>
+            </div>
+        </button>
+
+        <button class="flex justify-center rounded-[20px] w-[330px] h-[100px] bg-[#FA897B] text-white">
+            <div class="w-1/2 flex justify-center items-center">
+                <span class="font-bold text-2xl">マイログ</span>
+            </div>
+            <div class="w-1/2 flex justify-center items-center">
+                <span class="countlogs font-bold text-base">fff</span>
+                <span class="font-bold text-base">logs</span>
+            </div>
+        </button>
+
+    </div>
 </body>
